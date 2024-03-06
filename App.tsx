09edeb88f2dca053e749a80905c43fc05bdd5e8a
tusharsahu17 <tushar17sahu06@gmail.com>
2024-03-06 11:17:04 +0530

    remove ss

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ const TodoList = () => {
       {todos.length === 0 ? (
         <View style={style.container}>
           <View style={style.borderStyle} />
-          <Text style={style.textStyle}>No tasksss</Text>
+          <Text style={style.textStyle}>No tasks</Text>
           <View style={style.borderStyle} />
         </View>
       ) : (
